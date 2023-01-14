b6d3b2970db8ccbef636c6260cdc5955d3d55a12
m.osumi <m.osumi@goodpatch.com>
2023-01-14 18:24:16 +0900

    :+1: add tribial resolver for Photo

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+type Photo {
+  id: ID!
+  url: String!
+  name: String!
+  description: String
+}
 type Query {
   totalPhotos: Int!
+  allPhotos: [Photo!]!
 }
 type Mutation {
-  postPhoto(name: String! description: String): Boolean!
+  postPhoto(name: String! description: String): Photo!
 }
 `;
 
+let _id = 0;
 const photos = [];
 
 const resolvers = {
   Query: {
     totalPhotos: () => 42,
+    allPhotos: () => photos,
   },
   Mutation: {
     postPhoto(parent, args) {
-      photos.push(args);
-      return true;
+      const newPhoto = {
+        id: _id++,
+        ...args,
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     },
+  },
+  Photo: {
+    url: (parent) => `https://blog.cawpea.me/${parent.id}.jpg`,
   },
 };
 
