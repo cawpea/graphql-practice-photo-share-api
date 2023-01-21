034a1dfd8bd6186d2b1a900281fdca6390895cc3
m.osumi <m.osumi@goodpatch.com>
2023-01-21 17:57:32 +0900

    :+1: connect Photo and User using resolver

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+enum PhotoCategory {
+  SELFIE
+  PORTRAIT
+  ACTION
+  LANDSCAPE
+  GRAPHIC
+}
+type User {
+  githubLogin: ID!
+  name: String
+  avatar: String
+  postedPhotos: [Photo!]!
+  inPhotos: [Photo!]!
+}
 type Photo {
   id: ID!
   url: String!
   name: String!
+  description: String
+  category: PhotoCategory!
+  postedBy: User!
+  taggedUsers: [User!]!
+}
+input PostPhotoInput {
+  name: String!
+  category: PhotoCategory=PORTRAIT
   description: String
 }
 type Query {
@@ -12,12 +34,47 @@ type Query {
   allPhotos: [Photo!]!
 }
 type Mutation {
-  postPhoto(name: String! description: String): Photo!
+  postPhoto(input: PostPhotoInput!): Photo!
 }
 `;
 
 let _id = 0;
-const photos = [];
+
+var users = [
+  { githubLogin: "mHattrup", name: "Mike Hattrup" },
+  { githubLogin: "gPlake", name: "Glen Plake" },
+  { githubLogin: "sSchmidt", name: "Scot Schmidt" },
+];
+
+var photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+  },
+  {
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+  },
+];
+
+var tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" },
+];
 
 const resolvers = {
   Query: {
@@ -28,7 +85,7 @@ const resolvers = {
     postPhoto(parent, args) {
       const newPhoto = {
         id: _id++,
-        ...args,
+        ...args.input,
       };
       photos.push(newPhoto);
       return newPhoto;
@@ -36,6 +93,26 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `https://blog.cawpea.me/${parent.id}.jpg`,
+    postedBy: (parent) => {
+      return users.find((user) => user.githubLogin === parent.githubUser);
+    },
+    taggedUsers: (parent) => {
+      return tags
+        .filter((tag) => tag.photoID === parent.id)
+        .map((tag) => tag.userID)
+        .map((userID) => users.find((user) => user.githubLogin === userID));
+    },
+  },
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((photo) => photo.githubUser === parent.githubLogin);
+    },
+    inPhotos: (parent) => {
+      return tags
+        .filter((tag) => tag.userID === parent.id)
+        .map((tag) => tag.photoID)
+        .map((photoID) => photos.find((photo) => photo.id === photoID));
+    },
   },
 };
 
